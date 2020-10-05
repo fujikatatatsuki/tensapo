b313b45a39ea4d34e7577408d5cf3485f480a377
fujikatatatsuki <69786452+fujikatatatsuki@users.noreply.github.com>
2020-10-05 19:08:24 +0900

    Revert "Revert "タスク選択機能の実装""

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'costs#index'
+  resources :costs, only: [:new, :create]
+  #  post 'costs/new', to: 'costs#create'
+  # post 'posts', to: 'posts#create'
 end
